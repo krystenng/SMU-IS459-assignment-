696a55c2652a7496c6a03554397e75559c9922ac
krystenng <krystenng06@gmail.com>
2021-09-16 01:59:45 +0800

    assignment 2 scrapy

---
+++ assignment2/assignment2/pipelines.py
@@ -0,0 +1,34 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+import pymongo
+
+# # from scrapy.conf import settings
+# from scrapy.exceptions import DropItem
+# from scrapy import log
+
+class HardwarezonePipeline:
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            "localhost",
+            27017
+        )
+        db = connection["hardwarezone_1"]
+        self.collection = db["posts"]
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+        return item
