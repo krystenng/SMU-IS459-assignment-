53e8335086da945136e2f66b70265e334ffe906c
krystenng <79707828+krystenng@users.noreply.github.com>
2021-11-09 16:05:38 +0800

    Delete irrelevant codes

--- assignment3.py
+++ assignment3.py
@@ -1,8 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import explode
-from pyspark.sql.functions import split, concat_ws, substring, from_json, col, lower, regexp_replace, window, current_timestamp, desc
-from pyspark.ml.feature import StopWordsRemover, RegexTokenizer, Tokenizer
+from pyspark.sql.functions import from_json, col, window, current_timestamp, desc
 
 
 if __name__ == "__main__":
@@ -41,49 +40,21 @@ if __name__ == "__main__":
         .groupBy(window("timestamp", "2 minutes", "1 minute"), "author").count() \
         .withColumn("start", col("window")["start"]) \
         .withColumn("end", col("window")["end"]) \
-        .withColumn("current_timestamp", current_timestamp()) \
+        .withColumn("current_timestamp", current_timestamp()) 
+
+    top10_authorsDF = users_df \
         .filter(users_df.end < users_df.current_timestamp) \
         .orderBy(desc('window'), desc("count")).limit(10)
 
-    # remove stop words and punctuations
-
-    punct = lines.select('timestamp', split(lower(regexp_replace('content', r'[^\w\s]',''))," ").alias('content'))
-    stopwordList = (["\n","\t", "content", "click", "expand", "", " ", "..."])
-    stopwordList.extend(StopWordsRemover().getStopWords())
-    stopwordList = list(set(stopwordList))
-    remover = StopWordsRemover(inputCol='content', outputCol="cleaned_content", stopWords=stopwordList)
-    cleaned_df = remover.transform(punct)
-
-
-    # Splitting into individual words
-    cleaned_df = cleaned_df.select("timestamp", concat_ws(" ", "cleaned_content").alias("individual_word")) \
-        .select("timestamp", explode(split("individual_word", " ")).alias("content")) \
-        .groupBy(window("timestamp", "2 minutes", "1 minute"), "content").count() \
-        .withColumn("start", col('window')['start']) \
-        .withColumn("end", col('window')['end']) \
-        .withColumn("current_timestamp", current_timestamp()) \
-        .filter(cleaned_df.end < cleaned_df.current_timestamp)
-        .orderBy('window', 'count', ascending=False).limit(10)
-
     # #Select the content field and output
-    contents1 = users_df \
+    contents1 = top10_authorsDF \
         .writeStream \
         .queryName("WriteContent1") \
         .trigger(processingTime="1 minute") \
         .outputMode("complete") \
         .format("console") \
-        .option("checkpointLocation", "/user/chesteroby/spark-checkpoint1") \
-        .start()
-
-    #Select the content field and output
-    contents2 = cleaned_df \
-        .writeStream \
-        .queryName("WriteContent2") \
-        .trigger(processingTime="1 minute") \
-        .outputMode("complete") \
-        .format("console") \
-        .option("checkpointLocation", "/user/chesteroby/spark-checkpoint2") \
+        .option("checkpointLocation", "/user/krystenng/spark-checkpoint") \
         .start()
 
     #Start the job and wait for the incoming messages
-    contents2.awaitTermination()
+    contents1.awaitTermination()
